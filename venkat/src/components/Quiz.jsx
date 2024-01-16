47316e05df57e6f078a5731e7f46c8d45dce4cf2
Your Name <you@example.com>
2024-01-16 17:35:28 +0530

    ok

--- venkat/src/components/Quiz.jsx
+++ venkat/src/components/Quiz.jsx
@@ -3,23 +3,25 @@ import React from 'react'
 export default function Quiz() {
   return (
     <div className='Div'>
+     
         <div>
             <p>Question</p>
+            <p className='text'>1 of15</p>
             <p className='para'>Which is the only mammal that can jump</p>
             <div className='options'>
             <div className='op1'>
-            <div id='op'>
+            <div className='ops' id='op'>
               <p>Dog</p>
             </div>
-            <div id='op'>
+            <div className='ops2' id='op'>
               <p>Elephant</p>
             </div>
             </div>
             <div className='op1'> 
-            <div id='op'>
+            <div className='ops3' id='op'>
               <p>Goat</p>
             </div>
-            <div id='op'>
+            <div className='ops4' id='op'>
               <p>Lion</p>
             </div>
             </div>
